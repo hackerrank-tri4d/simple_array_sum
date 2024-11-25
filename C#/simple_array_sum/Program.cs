4eb9dd8aabed879867107dab25e856fa146cd268
Tri4d77 <simon.gabor@tri4d.hu>
2024-11-25 15:13:34 +0100

    C# Solution

---
+++ C#/simple_array_sum/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace simple_array_sum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> nums = new List<int>() {1, 2, 3, 4, 10, 11}
+        ;
+        Console.WriteLine(simpleArraySum);
+    }
+    
+    public static int simpleArraySum(List<int> ar)
+    {   
+        return ar.Sum();
+    }
+}
